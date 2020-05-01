4acb873b370de1fc33c5df8ebaf8f868e6171532
ellenlowing <lowing@bu.edu>
2020-05-01 14:09:43 -0400

    finish imsg container

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,17 +1,26 @@
 let mobileMode;
+let player;
+let playerState;
 
 $(document).ready(() => {
 	mobileMode = isMobile();
+	init();
 });
 
 function init() {
+	// Load Youtube API
+	var tag = document.createElement('script');
+	tag.src = "https://www.youtube.com/iframe_api";
+	var firstScriptTag = document.getElementsByTagName('script')[0];
+	firstScriptTag.parentNode.insertBefore(tag, firstScriptTag);
+
   if(mobileMode) {
-    $('iframe').css({
-      'transform': 'rotate(-90deg) translateX(+50vw) translateX(-50vh) translateY(-50vh) translateY(50vw)',
-			'-webkit-transform': 'rotate(-90deg) translateX(+50vw) translateX(-50vh) translateY(-50vh) translateY(50vw)',
-			'width': '100vh',
-			'height': '100vw'
-    })
+    // $('iframe').css({
+    //   'transform': 'rotate(-90deg) translateX(+50vw) translateX(-50vh) translateY(-50vh) translateY(50vw)',
+		// 	'-webkit-transform': 'rotate(-90deg) translateX(+50vw) translateX(-50vh) translateY(-50vh) translateY(50vw)',
+		// 	'width': '100vh',
+		// 	'height': '100vw'
+    // })
   }
 }
 
@@ -22,4 +31,20 @@ function isMobile() {
   } else {
     return false;
   }
+}
+
+function onYouTubeIframeAPIReady() {
+	player = new YT.Player('video', {
+	    events: {
+	      'onStateChange': onPlayerStateChange
+	    }
+	});
+}
+
+function onPlayerStateChange(event) {
+	playerState = event.data;
+	console.log(playerState);
+  // if (event.data == YT.PlayerState.PLAYING) {
+	//
+  // }
 }
